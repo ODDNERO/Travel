2d12566cb78190039a06b44b9d0c5aaa9f1cc663
NERO <mxzxro@gmail.com>
2024-05-27 08:07:29 +0900

    ✨ Storyboard 커스텀 셀 파일 추가

---
+++ Travel/TravelMagazineTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  TravelMagazineTableViewCell.swift
+//  Travel
+//
+//  Created by NERO on 5/27/24.
+//
+
+import UIKit
+
+class TravelMagazineTableViewCell: UITableViewCell {
+    @IBOutlet var magazineImageView: UIImageView!
+    @IBOutlet var magazineTitleLabel: UILabel!
+    @IBOutlet var magazineSubtitleLabel: UILabel!
+    @IBOutlet var magazineDateLabel: UILabel!
+}
