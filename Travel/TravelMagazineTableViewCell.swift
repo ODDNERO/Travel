557c948571d7cf61b9b4f213f384580da509d2ad
NERO <mxzxro@gmail.com>
2024-05-27 20:57:48 +0900

    💄 셀 UI 지정

--- Travel/TravelMagazineTableViewCell.swift
+++ Travel/TravelMagazineTableViewCell.swift
@@ -13,6 +13,27 @@ class TravelMagazineTableViewCell: UITableViewCell {
     @IBOutlet var magazineSubtitleLabel: UILabel!
     @IBOutlet var magazineDateLabel: UILabel!
     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        configureCellStyle()
+    }
+    
+    func configureCellStyle() {
+        magazineImageView.contentMode = .scaleAspectFill
+        magazineImageView.layer.cornerRadius = 15
+        
+        magazineTitleLabel.font = .systemFont(ofSize: 24, weight: .bold)
+        magazineTitleLabel.textColor = .black
+        magazineTitleLabel.numberOfLines = 0
+        
+        magazineSubtitleLabel.font = .systemFont(ofSize: 17, weight: .semibold)
+        magazineSubtitleLabel.textColor = .lightGray
+        
+        magazineDateLabel.font = .systemFont(ofSize: 15, weight: .semibold)
+        magazineDateLabel.textColor = .lightGray
+        magazineDateLabel.textAlignment = .right
+    }
+    
     func configureCell(data: Magazine) {
         let cellImage: String = data.photoURL
         let cellImageURL = URL(string: cellImage)
