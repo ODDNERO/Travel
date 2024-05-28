e3450976f1b90fc86233c3ef8c0ba4b07616a837
NERO <mxzxro@gmail.com>
2024-05-28 22:31:37 +0900

    ✨ Restaurant 셀 UI, 데이터 지정 메서드 추가

--- Travel/SearchRestaurant/RestaurantTableViewCell.swift
+++ Travel/SearchRestaurant/RestaurantTableViewCell.swift
@@ -20,5 +20,38 @@ class RestaurantTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        configureCellStyle()
+    }
+    
+    func configureCellStyle() {
+        foodCategotyButton.backgroundColor = .red
+        foodCategotyButton.tintColor = .white
+        foodCategotyButton.layer.cornerRadius = 10
+        
+        restaurantImageView.contentMode = .scaleAspectFill
+        restaurantImageView.layer.cornerRadius = 15
+        
+        restaurantNameLabel.font = .systemFont(ofSize: 18, weight: .bold)
+        restaurantNameLabel.textColor = .black
+        restaurantNameLabel.numberOfLines = 0
+        
+        restaurantAddressLabel.font = .systemFont(ofSize: 15, weight: .semibold)
+        restaurantAddressLabel.textColor = .black
+        restaurantNameLabel.numberOfLines = 0
+        
+        restaurantPriceLabel.font = .systemFont(ofSize: 13, weight: .regular)
+        restaurantPriceLabel.textColor = .lightGray
+    }
+    
+    func configureCell(data: Restaurant) {
+        let cellImage: String = data.image
+        let cellImageURL = URL(string: cellImage)
+        restaurantImageView.kf.setImage(with: cellImageURL)
+        
+        foodCategotyButton.setTitle(data.category, for: .normal)
+        restaurantNameLabel.text = data.name
+        restaurantAddressLabel.text = data.address
+        restaurantPriceLabel.text = data.price.formatted() + "원"
+        restaurantContactLabel.text = data.phoneNumber
     }
 }
