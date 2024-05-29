509e7a8253169e8acd431c22a04511f1a004b112
NERO <mxzxro@gmail.com>
2024-05-29 14:34:50 +0900

    💄 UIImages Enum 생성 및 CitySpot 셀 UI 지정

--- Travel/CitySpots/CitySpotsTableViewCell.swift
+++ Travel/CitySpots/CitySpotsTableViewCell.swift
@@ -15,9 +15,43 @@ class CitySpotsTableViewCell: UITableViewCell {
     @IBOutlet var starImageViewList: [UIImageView]!
     @IBOutlet var spotTitleDescriptionLabel: UILabel!
     @IBOutlet var heartButton: UIButton!
+    @IBOutlet var spotImageView: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        configureSpotUI()
+    }
+}
+
+//MARK: - UI Setting
+enum UIImages {
+    static let fillHeartImage = UIImage(systemName: "heart.fill")
+    static let emptyHeartImage = UIImage(systemName: "heart")
+    static let halfStarImage = UIImage(systemName: "star.leadinghalf.filled")
+    static let fillStarImage = UIImage(systemName: "star.fill")
+}
+
+extension CitySpotsTableViewCell {
+    func configureSpotUI() {
+        spotTitleLabel.font = .boldSystemFont(ofSize: 17)
+        spotTitleLabel.textColor = .black
         
+        spotDescriptionLabel.font = .systemFont(ofSize: 15)
+        spotDescriptionLabel.textColor = .darkGray
+        spotDescriptionLabel.textAlignment = .left
+        spotDescriptionLabel.numberOfLines = 0
+        
+        for grade in starImageViewList {
+            grade.image = UIImages.fillStarImage
+            grade.tintColor = .systemGray5
+        }
+        
+        spotTitleDescriptionLabel.textColor = .lightGray
+        spotTitleDescriptionLabel.font = .systemFont(ofSize: 14)
+        
+        spotImageView.contentMode = .scaleAspectFill
+        spotImageView.layer.cornerRadius = 8
+        
+        heartButton.tintColor = .white
     }
 }
