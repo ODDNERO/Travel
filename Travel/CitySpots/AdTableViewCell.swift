22a6eb541ebd8b25c7ad828bc3115f288e4e939c
NERO <mxzxro@gmail.com>
2024-05-29 13:06:22 +0900

    💄 ad 셀 레이아웃, 아웃렛 연결

---
+++ Travel/CitySpots/AdTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  AdTableViewCell.swift
+//  Travel
+//
+//  Created by NERO on 5/29/24.
+//
+
+import UIKit
+
+class AdTableViewCell: UITableViewCell {
+    @IBOutlet var adView: UIView!
+    @IBOutlet var adTitleLabel: UILabel!
+    @IBOutlet var adButton: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
