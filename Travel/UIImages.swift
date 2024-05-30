48402ccd6402b8524214d273d62a82cd3626dfc7
NERO <mxzxro@gmail.com>
2024-05-31 02:15:53 +0900

    ✨ UIImages enum 생성

---
+++ Travel/UIImages.swift
@@ -0,0 +1,13 @@
+
+import UIKit
+
+enum UIImages {
+    static let fillHeartImage = UIImage(systemName: "heart.fill")
+    static let emptyHeartImage = UIImage(systemName: "heart")
+    static let halfStarImage = UIImage(systemName: "star.leadinghalf.filled")
+    static let fillStarImage = UIImage(systemName: "star.fill")
+    static let backwardImage = UIImage(systemName: "chevron.backward")
+    static let forwardImage = UIImage(systemName: "chevron.forward")
+    static let shareImage = UIImage(systemName: "square.and.arrow.up")
+    static let xmarkImage = UIImage(systemName: "xmark")
+}
